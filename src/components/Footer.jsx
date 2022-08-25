a4662fe277ef0d58f36d241df3f45db035c9285f
Huichoman <rockbotica@gmail.com>
2022-08-25 15:11:28 -0500

    Component Footer added

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+export const Footer = () => {
+  return (
+    <footer>
+      <nav>
+        <ul>
+          <li>
+            <a href="https://github.com/Huichoman" rel="noopener noreferrer">
+              Linkedin
+            </a>
+          </li>
+        </ul>
+      </nav>
+      <p>By Huichoman</p>
+    </footer>
+  );
+};
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,21 @@
+import { Link } from "react-router-dom";
+
 export const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/listado">Listado</Link>
+          </li>
+          <li>
+            <Link to="/contacto">Contacto</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 };
